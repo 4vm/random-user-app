b934a0109c3ecd058801741243d102b7b9908a54
4vm <amadeumrzr@gmail.com>
2022-04-22 10:35:02 -0300

    add fetch function

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { useState } from 'react'
+import api from './services/api'
+
 const App = () => {
-  return <h1>Random</h1>
+  const [user, setUser] = useState([])
+
+  const fetchData = () => {
+    api
+      .get()
+      .then((response) => {
+        setUser(response.data.results[0])
+      })
+      .catch((error) => {
+        console.log('ERROR' + error)
+      })
+    console.log(user)
+  }
+  return (
+    <>
+      <button onClick={fetchData}>click</button>
+    </>
+  )
 }
 
 export default App
